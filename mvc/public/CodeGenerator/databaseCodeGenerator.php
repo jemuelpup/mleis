0d829d608c937e57d1c22fa2fbbcc3d467bf5ff8
jemuelpup <jemuelpup@gmail.com>
2017-07-07 17:00:35 +0800

    new

--- mvc/public/CodeGenerator/databaseCodeGenerator.php
+++ mvc/public/CodeGenerator/databaseCodeGenerator.php
@@ -1,15 +1,26 @@
 <?php
 
 class DatabaseCodeGenerator{
-
+	/*
+	'fieldName'
+	'required'
+	'defaultVal'
+	'dataType'
+	*/
 	// creates insert query for serialize array
 	public function createInsertQuery($tableData){
 		
 		$tableName = "";
 		
 		
-		
-		$query = "INSERT INTO division_tbl(id, name, active) VALUES ([value-1],[value-2],[value-3])";
+			
+		$fields = "";
+		foreach($tableData[1] as $fieldData){
+			$fields .= $fieldData['fieldName'].",";
+		}
+		$fields = substr($fields,0,-1);
+		$query = "INSERT INTO ".$tableData[0]['tableName']."($fields) VALUES ()";
+		echo $query;
 			
 			
 	}
@@ -20,7 +31,8 @@ class DatabaseCodeGenerator{
 	}
 
 	public function createUpdateQuery($tableData){
-
+		
+		$query = "UPDATE deduction_tbl SET id=[value-1],name=[value-2],active=[value-3],placeholder=[value-4],inModal=[value-5],taxDeductable=[value-6],deleted=[value-7] WHERE 1";
 	}
 	
 	
--- mvc/public/CodeGenerator/speedCoding.php
+++ mvc/public/CodeGenerator/speedCoding.php
@@ -8,19 +8,19 @@
 	$query = "
 	CREATE TABLE IF NOT EXISTS `tbl_overtime_request` (
 	`ot_request_id` int(11) NOT NULL,
-	  `ot_hours` int(11) NOT NULL COMMENT 'Minutes',
-	  `ot_timelog_id` int(11) NOT NULL,
-	  `ot_user_id` int(11) NOT NULL,
-	  `ot_filing_date` datetime NOT NULL,
-	  `ot_date` datetime NOT NULL,
-	  `ot_approval_date` date NOT NULL,
-	  `ot_reason` longtext NOT NULL,
-	  `ot_status_id` int(11) NOT NULL DEFAULT '3' COMMENT '3 = Pending | 2 = Disapproved | 1 = Approved',
-	  `ot_approveby_id` int(11) NOT NULL,
-	  `final_approved` int(11) NOT NULL DEFAULT '0' COMMENT '0 = pending | 1 = tl approved | 2 = manager approved',
-	  `ot_token` varchar(100) DEFAULT 'JEMUEL, ELIMANCO' NOT NULL,
-	  `startTime` datetime NOT NULL,
-	  `endTime` datetime NOT NULL
+	`ot_hours` int(11) NOT NULL COMMENT 'Minutes',
+	`ot_timelog_id` int(11) NOT NULL,
+	`ot_user_id` int(11) NOT NULL,
+	`ot_filing_date` datetime NOT NULL,
+	`ot_date` datetime NOT NULL,
+	`ot_approval_date` date NOT NULL,
+	`ot_reason` longtext NOT NULL,
+	`ot_status_id` int(11) NOT NULL DEFAULT '3' COMMENT '3 = Pending | 2 = Disapproved | 1 = Approved',
+	`ot_approveby_id` int(11) NOT NULL,
+	`final_approved` int(11) NOT NULL DEFAULT '0' COMMENT '0 = pending | 1 = tl approved | 2 = manager approved',
+	`ot_token` varchar(100) DEFAULT 'JEMUEL, ELIMANCO' NOT NULL,
+	`startTime` datetime NOT NULL,
+	`endTime` datetime NOT NULL
 	) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 	";
 	$tableData = [];
@@ -30,6 +30,7 @@
 	$qf = new QueryFormatter;
 	$htmlCG = new HTMLCodeGenerator;
 	$phpCodeGenerator = new PHPCodeGenerator;
+	$dbcg = new DatabaseCodeGenerator;
 
 	// Operations
 	$tableData = $qf->formatQuery($query);
@@ -38,7 +39,9 @@
 
 
 	if(count($tableData)){
-		$htmlCG->generateInputFields($tableData,true,false);
+//		$htmlCG->generateInputFields($tableData,true,false);
+		echo $dbcg->createInsertQuery($tableData);
+		
 	}
 
 
